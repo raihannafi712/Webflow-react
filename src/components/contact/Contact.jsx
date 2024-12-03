30f69100af320c91b0ba9b144a20134e94caafd9
raihannafi712 <shitabraihan712@gmail.com>
2024-12-03 16:54:16 +0600

    demo1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container, Row ,Col } from 'react-bootstrap'
 import './contact.css'
+import { Container ,Row , Col } from 'react-bootstrap'
 
 const Contact = () => {
   return (
